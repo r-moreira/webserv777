16ac5dd0779e150d7ad19d0c020bd97116886c46
Rodrigo Avila <r.moreira4297@gmail.com>
2023-03-20 19:02:34 -0300

    Feature: Adding response header content-length

--- srcs/webserv.cpp
+++ srcs/webserv.cpp
@@ -2,14 +2,14 @@
 #include "../includes/parser/HttpRequestParser.h"
 #include "../includes/parser/UrlParser.h"
 
-std::string fileExtension[] = {
+std::string file_extension[] = {
         "aac", "avi", "bmp", "css", "gif", "ico", "js",
         "json", "mp3", "mp4", "otf", "png", "php", "rtf",
         "svg", "txt", "webm", "webp", "woff", "woff", "zip",
         "html", "htm", "jpeg", "jpg",
 };
 
-std::string ContentType[] = {
+std::string content_type[] = {
         "Content-Type: audio/aac\r\n\r\n",
         "Content-Type: video/x-msvideo\r\n\r\n",
         "Content-Type: image/bmp\r\n\r\n",
@@ -37,7 +37,7 @@ std::string ContentType[] = {
         "Content-Type: image/jpeg\r\n\r\n",
 };
 
-std::string Messages[] = {
+std::string messages[] = {
         "HTTP/1.1 200 Ok\r\n ",
         "HTTP/1.0 400 Bad Request\r\nContent-Type: text/html\r\n\r\n<!doctype html><html><body>System is busy right now.</body></html>",
         "HTTP/1.0 404 File not found\r\nContent-Type: text/html\r\n\r\n<!doctype html><html><body>The reuested file does not exist on this server</body></html>"
@@ -90,26 +90,31 @@ void set_non_blocking(int fd) {
     }
 }
 
-std::string getHeaders(std::string file_path) {
+std::string getHeaders(const std::string& file_path, size_t file_size) {
+    char size_t_byte_buffer[25] = {};
     std::string headers = "HTTP/1.1 200 Ok\r\n";
-    std::string extension = file_path.substr(file_path.find_last_of(".") + 1);
+
+    if (snprintf(size_t_byte_buffer, 25, "%lu", file_size) < 0) {
+        std::cerr << RED << "Error while formatting file size: " << strerror(errno) << RESET << std::endl;
+        //stop connection, display error page
+    }
+    std::string content_length = "Content-Length: " + std::string(size_t_byte_buffer) + "\r\n";
+
+    std::string extension = file_path.substr(file_path.find_last_of('.') + 1);
     for (int i = 0; i < 25; i++) {
-        if (extension == fileExtension[i]) {
-            headers += ContentType[i];
+        if (extension == file_extension[i]) {
+            headers += content_length;
+            headers += content_type[i];
             return headers;
         }
     }
-    return "HTTP/1.1 200 Ok\r\nContent-Type: text/html\r\n\r\n";
+    return "HTTP/1.1 200 Ok\r\n" + content_length + "Content-Type: text/html\r\n\r\n";
 }
 
 void write_response(event_data_t *event_data) {
     UrlParser urlParser;
     std::string file_path = "./public" + event_data->request.getUri();
 
-    if (send(event_data->client_fd, getHeaders(file_path).c_str(), getHeaders(file_path).size(), 0) < 0) {
-        std::cerr << RED << "Error while writing status header to client: " << strerror(errno) << RESET << std::endl;
-        //return error page, end connection
-    }
 
     std::ifstream file;
     file.open(file_path.c_str(), std::ios::in | std::ios::binary);
@@ -117,7 +122,14 @@ void write_response(event_data_t *event_data) {
     std::string contents((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
     std::cout << YELLOW << " Transmission Data Size " << contents.length() << " Bytes." << RESET << std::endl;
 
-    std::cout << YELLOW << "Sending..." << RESET << std::endl;
+    std::string headers = getHeaders(file_path, contents.length());
+
+    std::cout << CYAN << "Response Headers:\n" << headers << RESET << std::endl;
+
+    if (send(event_data->client_fd, headers.c_str(), headers.size(), 0) < 0) {
+        std::cerr << RED << "Error while writing status header to client: " << strerror(errno) << RESET << std::endl;
+        //return error page, end connection
+    }
 
     long bytes_sent = send(event_data->client_fd , contents.c_str() , contents.length() , 0 );
     if (bytes_sent < 0) {
@@ -241,7 +253,6 @@ void io_multiplexing_event_loop(int server_socket_fd) {
                 event_data->client_fd = client_fd;
                 event_data->read_bytes = 0;
                 event_data->event_status = Reading;
-                event_data->read_left = 2; //TEMPORÁRIO
 
                 request_event.data.ptr = event_data;
                 request_event.events = EPOLLIN;
