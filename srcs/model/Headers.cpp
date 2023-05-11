49509ef29d948630f7093fabc3db158317c99143
Rodrigo Avila <r.moreira4297@gmail.com>
2023-05-11 19:06:07 -0300

    Melhorando get file headers

--- srcs/model/Headers.cpp
+++ srcs/model/Headers.cpp
@@ -10,17 +10,16 @@ Headers::~Headers() {}
 
 std::string Headers::getFileHeaders(const std::string& file_path, size_t file_size) {
     char size_t_byte_buffer[25] = {};
-    std::string headers = "HTTP/1.1 200 Ok\r\n";
-
-    if (snprintf(size_t_byte_buffer, 25, "%lu", file_size) < 0) {
-        std::cerr << RED << "Error while formatting file size: " << strerror(errno) << RESET << std::endl;
-        //stop connection, display error page
-    }
-    std::string content_length = "Content-Length: " + std::string(size_t_byte_buffer) + "\r\n";
 
     std::string extension = file_path.substr(file_path.find_last_of('.') + 1);
 
-    return "HTTP/1.1 200 Ok\r\n" + content_length + "Content-Type: " + this->_mime_types.get_mime_type(extension) +"\r\n\r\n";
+    if (snprintf(size_t_byte_buffer, 25, "%lu", file_size) < 0) {
+        std::cerr << RED << "Error while formatting file size: " << strerror(errno) << RESET << std::endl;
+        return "HTTP/1.1 200 Ok\r\nContent-Type: " + this->_mime_types.get_mime_type(extension) + "\r\n\r\n";
+    }
+
+    std::string content_length = "Content-Length: " + std::string(size_t_byte_buffer) + "\r\n";
+    return "HTTP/1.1 200 Ok\r\n" + content_length + "Content-Type: " + this->_mime_types.get_mime_type(extension) + "\r\n\r\n";
 }
 
 std::string Headers::getErrorHeaders(event_http_status_enum_t status) {
