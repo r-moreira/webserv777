b165f82acd3eb831b5e2bad6457c23cad15cb949
Alef <alefgabrielr@gmail.com>
2023-06-10 14:21:51 -0300

    feat: add bash script

---
+++ public/cgi/test.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "-------------------------"
+echo "CGI Environment Variables:"
+echo "-------------------------"
+echo "HTTP_USER_AGENT: $HTTP_USER_AGENT"
+echo "REMOTE_ADDR: $REMOTE_ADDR"
+echo "SERVER_NAME: $SERVER_NAME"
+echo "SERVER_PORT: $SERVER_PORT"
+echo "QUERY_STRING: $QUERY_STRING"
+echo "REQUEST_METHOD: $REQUEST_METHOD"
+echo "SCRIPT_NAME: $SCRIPT_NAME"
+echo "SCRIPT_FILENAME: $SCRIPT_FILENAME"
+echo "SERVER_PROTOCOL: $SERVER_PROTOCOL"
